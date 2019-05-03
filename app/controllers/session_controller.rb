400502a7a02338f76a8ffa60abce06867f17be09
azdren ymeri <azdrenymeri@outlook.com>
2019-05-03 16:50:11 +0200

    small fixes

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,7 +6,7 @@ class SessionController < ApplicationController
     def create
         @user = User.find_by_name(params[:user_name])
         session[:user_id] = @user.id
-        redirect_to root_url
+        redirect_to events_path
     end
     def destroy
         session[:user_id] = nil
