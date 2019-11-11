8c6ac28125ea62ea27c4e2bea52992da6ae13b18
Milos Vukadinovic <milos.vukadinovic11@gmail.com>
2019-11-11 23:51:45 +0100

    adding password, prepping for some design

--- db/schema.rb
+++ db/schema.rb
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_132501) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
